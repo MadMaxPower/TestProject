68d0f886f45a5c17de446e7633a4fbce024b3b42
Абакумов Максим Евгеньевич <abakumov-m@da.lan>
2023-07-05 19:16:05 +0300

    file test.py deleted

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dir_name =os.path.dirname(os.path.abspath(__file__))
-pth_to_file = os.path.join(dir_name, 'main.py')
-# print(pth_to_file)
-
-os_command = f'python {pth_to_file}'
-
-os.system(os_command)
